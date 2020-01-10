2153013fcf2830453c28f53cf22f1ef947901f00
Peter Saxton <peterhsaxton@gmail.com>
2020-01-10 13:37:19 +0000

    configure issuer to discover from well-known endpoint

--- examples/express_js/server/index.js
+++ examples/express_js/server/index.js
@@ -3,6 +3,20 @@ const { Issuer } = require("openid-client");
 
 const port = 3000;
 
+var client;
+var issuer;
+Issuer.discover("http://localhost:8080/.well-known/openid-configuration") // => Promise
+  .then(function(i) {
+    issuer = i;
+    console.log("Discovered issuer %s %O", issuer.issuer, issuer.metadata);
+
+    client = new issuer.Client({
+      client_id: "http://localhost:3000/callback",
+      response_types: ["id_token"],
+      id_token_signed_response_alg: "ES256"
+    });
+  });
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -11,7 +25,10 @@ app.get("/", function(req, res) {
   res.render("index", {});
 });
 app.get("/sign-in", function(req, res) {
-  res.redirect("http://localhost:8080/wallet");
+  authUrl = client.authorizationUrl({
+    scope: "openid"
+  });
+  res.redirect(authUrl);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
